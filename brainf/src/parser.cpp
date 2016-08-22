71ed48611fb1b0893c7f67ec9aa6fa516aacc317
Oopsno <yufeiliu94@gmail.com>
2016-08-22 23:09:51 +0800

    update parser to support operands

--- brainf/src/parser.cpp
+++ brainf/src/parser.cpp
@@ -10,21 +10,21 @@ std::vector<Operator> tokenize(const std::string &code) {
   for (size_t offset = 0; offset < code.length(); ++offset) {
     switch (code[offset]) {
       #define SAME_WITH_LATEST(t) (not os.empty() and os.back().type == OperatorType::t)
-      #define PUSH(t) os.push_back({OperatorType::t, 1, {lineno, column, offset}, {lineno, column, offset}})
-      #define APPEND(t) \
+      #define PUSH(t, opr) os.push_back({OperatorType::t, {OperandType::opr}, 1, {lineno, column, offset}, {lineno, column, offset}})
+      #define APPEND(t, opr) \
         const auto begin = os.back().begin; \
         const auto repeat = os.back().repeat; \
         os.pop_back(); \
-        os.push_back({OperatorType::t, repeat + 1, begin, {lineno, column, offset}})
-      #define MATCH(t) case static_cast<char>(OperatorType::t): if (SAME_WITH_LATEST(t)) { APPEND(t); } else { PUSH(t); } break
-      MATCH(Increase);
-      MATCH(Decrease);
-      MATCH(MoveLeft);
-      MATCH(MoveRight);
-      MATCH(Read);
-      MATCH(Print);
-      MATCH(LoopBegin);
-      MATCH(LoopEnd);
+        os.push_back({OperatorType::t, {OperandType::opr}, repeat + 1, begin, {lineno, column, offset}})
+      #define MATCH(t, opr) case static_cast<char>(OperatorType::t): if (SAME_WITH_LATEST(t)) { APPEND(t, opr); } else { PUSH(t, opr); } break
+      MATCH(Increase, DynamicUnit);
+      MATCH(Decrease, DynamicUnit);
+      MATCH(MoveLeft, Pointer);
+      MATCH(MoveRight, Pointer);
+      MATCH(Read, DynamicUnit);
+      MATCH(Print, DynamicUnit);
+      MATCH(LoopBegin, DynamicUnit);
+      MATCH(LoopEnd, DynamicUnit);
       case '\r':
       case '\n':
         lineno += 1;
