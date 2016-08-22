1e40dbb69083d04788862ae72cd90bc6cbc7c419
Oopsno <yufeiliu94@gmail.com>
2016-08-22 23:09:19 +0800

    add operand to each operator

--- brainf/include/operator.hpp
+++ brainf/include/operator.hpp
@@ -17,16 +17,27 @@ enum class OperatorType: char {
   LoopEnd   = ']'
 };
 
+enum class OperandType {
+  DynamicUnit, PredictedUnit, Pointer
+};
+
 struct Position {
   size_t offset, line, column;
 };
 
+struct Operand {
+  OperandType type;
+  uint32_t value = 0;
+};
+
 struct Operator {
   OperatorType type;
+  Operand operand;
   size_t repeat;
   Position begin, end;
 };
 
+std::ostream &operator<<(std::ostream &os, const Operand &o);
 std::ostream &operator<<(std::ostream &os, const Operator &o);
 std::ostream &operator<<(std::ostream &os, const Position &p);
 
--- brainf/src/operator.cpp
+++ brainf/src/operator.cpp
@@ -3,12 +3,26 @@
 namespace EsoFarm {
 namespace BrainF {
 
+std::ostream &operator<<(std::ostream &os, const Operand &o) {
+  switch (o.type) {
+    case OperandType::DynamicUnit:
+      os << "U";
+      break;
+    case OperandType::PredictedUnit:
+      os << "#" << o.value;
+      break;
+    default:
+      os << "P";
+  }
+  return os;
+}
+
 std::ostream &operator<<(std::ostream &os, const Position &p) {
   return os << p.line << ":" << p.column << " (+" << p.offset << ")";
 }
 
 std::ostream &operator<<(std::ostream &os, const Operator &o) {
-  return os << o.repeat << " " << (char) (o.type) << " [From " << o.begin << " To " << o.end << "]";
+  return os << o.repeat << " " << (char) (o.type) << " " << o.operand << " [From " << o.begin << " To " << o.end << "]";
 }
 
 }
