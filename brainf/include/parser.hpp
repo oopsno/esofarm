20f482f09a3b4ef1a5f04ff3e5abfe6b3f86b7e5
Oopsno <yufeiliu94@gmail.com>
2016-08-23 09:30:35 +0800

    complete parser
    + fixed problem in tokenize()
    + added syntax checker
    + added token -> AST parser

--- brainf/include/parser.hpp
+++ brainf/include/parser.hpp
@@ -3,11 +3,22 @@
 #include <string>
 #include <vector>
 #include "operator.hpp"
+#include "ast.hpp"
 
 namespace EsoFarm {
 namespace BrainF {
 
+class SyntaxError: std::exception {
+ public:
+  SyntaxError(const std::string &what, const Position &where) : what(what), where(where) { }
+  const std::string what;
+  const Position where;
+};
+
 std::vector<Operator> tokenize(const std::string &code);
+bool syntax_check(const std::vector<Operator> &tokens) throw(SyntaxError);
+std::vector<AST *> parser(const std::vector<Operator> &opr);
+
 
 }
 }
--- brainf/src/parser.cpp
+++ brainf/src/parser.cpp
@@ -1,4 +1,5 @@
 #include "parser.hpp"
+#include <stack>
 
 namespace EsoFarm {
 namespace BrainF {
@@ -16,25 +17,90 @@ std::vector<Operator> tokenize(const std::string &code) {
         const auto repeat = os.back().repeat; \
         os.pop_back(); \
         os.push_back({OperatorType::t, {OperandType::opr}, repeat + 1, begin, {lineno, column, offset}})
-      #define MATCH(t, opr) case static_cast<char>(OperatorType::t): if (SAME_WITH_LATEST(t)) { APPEND(t, opr); } else { PUSH(t, opr); } break
-      MATCH(Increase, DynamicUnit);
-      MATCH(Decrease, DynamicUnit);
-      MATCH(MoveLeft, Pointer);
-      MATCH(MoveRight, Pointer);
-      MATCH(Read, DynamicUnit);
-      MATCH(Print, DynamicUnit);
+      #define COUNT(t, opr) case static_cast<char>(OperatorType::t): if (SAME_WITH_LATEST(t)) { APPEND(t, opr); } else { PUSH(t, opr); } break
+      #define MATCH(t, opr) case static_cast<char>(OperatorType::t): PUSH(t, opr); break
+      COUNT(Increase, DynamicUnit);
+      COUNT(Decrease, DynamicUnit);
+      COUNT(MoveLeft, Pointer);
+      COUNT(MoveRight, Pointer);
+      COUNT(Read, DynamicUnit);
+      COUNT(Print, DynamicUnit);
       MATCH(LoopBegin, DynamicUnit);
       MATCH(LoopEnd, DynamicUnit);
-      case '\r':
       case '\n':
         lineno += 1;
         column = 1;
         break;
       default:
         continue;
+      #undef MATCH
+      #undef COUNT
+      #undef APPEND
+      #undef PUSH
+      #undef SAME_WITH_LATEST
     }
   }
   return os;
+}
+
+bool syntax_check(const std::vector<Operator> &tokens) throw(SyntaxError) {
+  std::stack<const Operator *> s;
+  for (const auto &t : tokens) {
+    switch (t.type) {
+      case OperatorType::LoopBegin:
+        s.push(&t);
+        break;
+      case OperatorType::LoopEnd:
+        if (s.empty()) {
+          throw SyntaxError{"Unexpected `['", t.begin};
+        } else {
+          s.pop();
+        }
+        break;
+      default:
+        continue;
+    }
+  }
+  if (not s.empty()) {
+    throw SyntaxError{"Unexpected EOF: `[' not matched", s.top()->begin};
+  };
+  return true;
+}
+
+std::vector<AST *> parser(const std::vector<Operator> &opr) {
+  std::vector<AST *> asts{};
+  std::stack<std::vector<AST *> *> stack{};
+  std::vector<AST *> *curr_block = &asts;
+  for (auto i = 0; i < opr.size(); ++i) {
+    auto const &o = opr[i];
+    switch (o.type) {
+      case OperatorType::Decrease:
+      case OperatorType::Increase:
+        curr_block->push_back(new Arithmetic{o});
+        break;
+      case OperatorType::MoveLeft:
+      case OperatorType::MoveRight:
+        curr_block->push_back(new Pointer{o});
+        break;
+      case OperatorType::Read:
+        curr_block->push_back(new Read{o});
+        break;
+      case OperatorType::Print:
+        curr_block->push_back(new Print{o});
+        break;
+      case OperatorType::LoopBegin:
+        curr_block->push_back(new Loop{o});
+        stack.push(curr_block);
+        curr_block = &((Loop *) (curr_block->back()))->getBlock();
+        break;
+      case OperatorType::LoopEnd:
+        curr_block = stack.top();
+        stack.pop();
+        ((Loop *)(curr_block->back()))->setLoopEnd(o);
+        break;
+    }
+  }
+  return asts;
 }
 
 }
