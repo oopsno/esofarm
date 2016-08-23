3579a1ef2760161a178dbd12667c4dff7470b6ff
Oopsno <yufeiliu94@gmail.com>
2016-08-23 17:31:32 +0800

    add Ptr/Unit manager & debug mode

--- brainf/src/codegen/llvm.cpp
+++ brainf/src/codegen/llvm.cpp
@@ -7,8 +7,12 @@ using namespace llvm;
 namespace EsoFarm {
 namespace BrainF {
 
-template <typename Fn, GlobalValue::LinkageTypes Link>
-static Function *makeFunction(LLVMContext &c, Module *m, IRBuilder<> &b, std::string name, std::vector<std::string> arg_names) {
+template<typename Fn, GlobalValue::LinkageTypes Link>
+static Function *makeFunction(LLVMContext &c,
+                              Module *m,
+                              IRBuilder<> &b,
+                              std::string name,
+                              std::vector<std::string> arg_names) {
   auto *fn_t = TypeBuilder<Fn, false>::get(c);
   Function *fn = Function::Create(fn_t, Link, name, m);
   auto i = 0;
@@ -31,27 +35,31 @@ LLVMGenerator::LLVMGenerator(const std::string &name) : ctx{}, builder{ctx}, mod
   memset = Intrinsic::getDeclaration(module.get(), Intrinsic::memset, {i8PtrTy, i64Ty});
   auto puts = cast<Function>(module->getOrInsertFunction("puts", i32Ty, i8PtrTy, nullptr));
 
-  // Create Function `void DMSG(i8 *message)'
-  dmsg = makeFunction<void(int8_t*), Function::InternalLinkage>(ctx, module.get(), builder, "DMSG", {"message"});
-  auto *dmsg_bb = BasicBlock::Create(ctx, "", dmsg);
-  builder.SetInsertPoint(dmsg_bb);
-  Value *msg = &*dmsg->arg_begin();
-  builder.CreateCall(puts, msg);
-  builder.CreateRetVoid();
+  if (debugMode) {
+    // Create Function `void DMSG(i8 *message)'
+    dmsg = makeFunction<void(int8_t *), Function::InternalLinkage>
+        (ctx, module.get(), builder, "DMSG", {"message"});
+    auto *dmsg_bb = BasicBlock::Create(ctx, "", dmsg);
+    builder.SetInsertPoint(dmsg_bb);
+    Value *msg = &*dmsg->arg_begin();
+    builder.CreateCall(puts, msg);
+    builder.CreateRetVoid();
 
-  // Create Function `void DST(i8 unit, i32 ptr)'
-  auto *printf_t = TypeBuilder<int(char *, ...), false>::get(ctx);
-  auto *printf = cast<Function>(module->getOrInsertFunction("printf", printf_t));
-  dst = makeFunction<void(int8_t, int32_t), Function::InternalLinkage>(ctx, module.get(), builder, "DST", {"uval", "pval"});
-  auto argi = dst->arg_begin();
-  Value *f_unit = &*argi;
-  argi++;
-  Value *f_ptr = &*argi;
-  auto dst_bb = BasicBlock::Create(ctx, "", dst);
-  builder.SetInsertPoint(dst_bb);
-  auto *fmt = builder.CreateGlobalStringPtr("ST P = %d U = %d\n");
-  builder.CreateCall(printf, {fmt, f_ptr, f_unit});
-  builder.CreateRetVoid();
+    // Create Function `void DST(i8 unit, i32 ptr)'
+    auto *printf_t = TypeBuilder<int(char *, ...), false>::get(ctx);
+    auto *printf = cast<Function>(module->getOrInsertFunction("printf", printf_t));
+    dst = makeFunction<void(int8_t, int32_t), Function::InternalLinkage>
+        (ctx, module.get(), builder, "DST", {"uval", "pval"});
+    auto argi = dst->arg_begin();
+    Value *f_unit = &*argi;
+    argi++;
+    Value *f_ptr = &*argi;
+    auto dst_bb = BasicBlock::Create(ctx, "", dst);
+    builder.SetInsertPoint(dst_bb);
+    auto *fmt = builder.CreateGlobalStringPtr("ST P = %d U = %d\n");
+    builder.CreateCall(printf, {fmt, f_ptr, f_unit});
+    builder.CreateRetVoid();
+  }
 
   // Create Function `i32 BrainF(i8 *units)'
   std::vector<Type *> brainf_args{i8PtrTy};
@@ -77,7 +85,8 @@ LLVMGenerator::LLVMGenerator(const std::string &name) : ctx{}, builder{ctx}, mod
   alloc->setName("units");
   main_bs->getInstList().push_back(alloc);
   // memset(units, 0, 30000);
-  auto init = builder.CreateCall(memset, {alloc, builder.getInt8(0), builder.getInt64(30000), builder.getInt32(1), builder.getInt1(true)});
+  auto init = builder.CreateCall
+      (memset, {alloc, builder.getInt8(0), builder.getInt64(30000), builder.getInt32(1), builder.getInt1(true)});
   // BrainF(units)
   auto callbf = builder.CreateCall(bf, {alloc});
   // free units;
@@ -91,91 +100,105 @@ LLVMGenerator::LLVMGenerator(const std::string &name) : ctx{}, builder{ctx}, mod
   builder.SetInsertPoint(bs);
 }
 
-Value * LLVMGenerator::on_arithmetic(Arithmetic *ast) {
-  auto *pval = builder.CreateLoad(i32Ty, ptr);
-  auto *unit = builder.CreateGEP(units, pval);
-  auto *uval = builder.CreateLoad(i8Ty, unit);
-  Value *result, *dbg_msg;
+Value *LLVMGenerator::on_arithmetic(Arithmetic *ast) {
+  Value *result;
+  char opr = '+';
   if (ast->opr.type == OperatorType::Increase) {
-    dbg_msg = builder.CreateGlobalStringPtr(std::to_string(ast->opr.repeat) + "+");
-    result = builder.CreateAdd(uval, builder.getInt8(int8_t(ast->opr.repeat)));
+    result = builder.CreateAdd(getCurrentUnit(), builder.getInt8(int8_t(ast->opr.repeat)));
   } else {
-    dbg_msg = builder.CreateGlobalStringPtr(std::to_string(ast->opr.repeat) + "-");
-    result = builder.CreateSub(uval, builder.getInt8(int8_t(ast->opr.repeat)));
+    opr = '-';
+    result = builder.CreateSub(getCurrentUnit(), builder.getInt8(int8_t(ast->opr.repeat)));
   }
-  builder.CreateCall(dmsg, dbg_msg);
-  builder.CreateStore(result, unit);
-  builder.CreateCall(dst, {result, pval});
+  if (debugMode) {
+    auto *dbg_msg = builder.CreateGlobalStringPtr(std::to_string(ast->opr.repeat) + opr);
+    builder.CreateCall(dmsg, dbg_msg);
+    builder.CreateCall(dst, {result, getPtr()});
+  }
+  builder.CreateStore(result, getCurrentUnitPtr());
+  unitUpdated();
   return nullptr;
 }
 
-Value * LLVMGenerator::on_pointer(Pointer *ast) {
-  auto *pval = builder.CreateLoad(i32Ty, ptr);
-  Value *result, *dbg_msg;
+Value *LLVMGenerator::on_pointer(Pointer *ast) {
+  Value *result;
+  char opr = '>';
   if (ast->opr.type == OperatorType::MoveRight) {
-    dbg_msg = builder.CreateGlobalStringPtr(std::to_string(ast->opr.repeat) + ">");
-    result = builder.CreateAdd(pval, builder.getInt32(ast->opr.repeat));
+    result = builder.CreateAdd(getPtr(), builder.getInt32(ast->opr.repeat));
   } else {
-    dbg_msg = builder.CreateGlobalStringPtr(std::to_string(ast->opr.repeat) + "<");
-    result = builder.CreateSub(pval, builder.getInt32(ast->opr.repeat));
+    opr = '<';
+    result = builder.CreateSub(getPtr(), builder.getInt32(ast->opr.repeat));
   }
-  builder.CreateCall(dmsg, dbg_msg);
+  if (debugMode) {
+    auto *dbg_msg = builder.CreateGlobalStringPtr(std::to_string(ast->opr.repeat) + opr);
+    builder.CreateCall(dmsg, dbg_msg);
+    builder.CreateCall(dst, {getCurrentUnit(), getPtr()});
+  }
   builder.CreateStore(result, ptr);
-  auto *unit = builder.CreateInBoundsGEP(units, result);
-  auto *uval = builder.CreateLoad(i8Ty, unit);
-  builder.CreateCall(dst, {uval, result});
+  pointerUpdated();
   return nullptr;
 }
 
 Value *LLVMGenerator::on_read(Read *ast) {
-  auto *r = builder.CreateGlobalStringPtr("READ");
-  builder.CreateCall(dmsg, r);
-  auto *pval = builder.CreateLoad(i32Ty, ptr);
-  auto *unit = builder.CreateInBoundsGEP(units, pval);
+  if (debugMode) {
+    auto *r = builder.CreateGlobalStringPtr("READ");
+    builder.CreateCall(dmsg, r);
+  }
   for (auto i = 1; i < ast->opr.repeat; ++i) {
     builder.CreateCall(read, None);
   }
   auto *result_i32 = builder.CreateCall(read, None);
   auto *result_i8 = builder.CreateIntCast(result_i32, i8Ty, true);
-  builder.CreateStore(result_i8, unit);
-  builder.CreateCall(dst, {result_i8, pval});
-  return nullptr;
-}
-
-Value * LLVMGenerator::on_print(Print *ast) {
-  auto *pval = builder.CreateLoad(i32Ty, ptr);
-  auto *unit = builder.CreateInBoundsGEP(units, pval);
-  auto *uval = builder.CreateLoad(i8Ty, unit);
-  auto *uval_i32 = builder.CreateIntCast(uval, i32Ty, true);
-  for (auto i = 0; i < ast->opr.repeat; ++i) {
-    builder.CreateCall(print, {uval_i32});
+  builder.CreateStore(result_i8, getCurrentUnitPtr());
+  unitUpdated();
+  if (debugMode) {
+    builder.CreateCall(dst, {result_i8, getPtr()});
   }
   return nullptr;
 }
 
-Value * LLVMGenerator::on_loop(Loop *ast) {
+Value *LLVMGenerator::on_print(Print *ast) {
+  if (debugMode) {
+    auto *r = builder.CreateGlobalStringPtr("PRINT");
+    builder.CreateCall(dmsg, r);
+  }
+  auto *uval_i32 = builder.CreateIntCast(getCurrentUnit(), i32Ty, true);
+  for (auto i = 0; i < ast->opr.repeat; ++i) {
+    builder.CreateCall(print, uval_i32);
+  }
+  return nullptr;
+}
+
+Value *LLVMGenerator::on_loop(Loop *ast) {
   auto *func = builder.GetInsertBlock()->getParent();
-  auto name_suffix = std::to_string(ast->opr.begin.offset) + "_" + std::to_string(ast->end.end.offset);
+  const auto name_suffix = std::to_string(ast->opr.begin.offset) + "_" + std::to_string(ast->end.end.offset);
   auto *guard = BasicBlock::Create(ctx, "guard_" + name_suffix, func);
   auto *loop = BasicBlock::Create(ctx, "loop_" + name_suffix, func);
   auto *after = BasicBlock::Create(ctx, "afterloop_" + name_suffix, func);
+  pointerUpdated();
   builder.CreateBr(guard);
   builder.SetInsertPoint(guard);
-  builder.CreateCall(dmsg, builder.CreateGlobalStringPtr(": guard_" + name_suffix));
-  auto *pval = builder.CreateLoad(i32Ty, ptr);
-  auto *unit = builder.CreateInBoundsGEP(units, pval);
-  auto *uval = builder.CreateLoad(i8Ty, unit);
-  auto *cond = builder.CreateICmpNE(uval, builder.getInt8(0));
-  builder.CreateCall(dst, {uval, pval});
+  if (debugMode) {
+    builder.CreateCall(dmsg, builder.CreateGlobalStringPtr(": guard_" + name_suffix));
+  }
+  auto *cond = builder.CreateICmpNE(getCurrentUnit(), builder.getInt8(0));
+  if (debugMode) {
+    builder.CreateCall(dst, {getCurrentUnit(), getPtr()});
+  }
+  pointerUpdated();
   builder.CreateCondBr(cond, loop, after);
   builder.SetInsertPoint(loop);
-  builder.CreateCall(dmsg, builder.CreateGlobalStringPtr(": loop_" + name_suffix));
+  if (debugMode) {
+    builder.CreateCall(dmsg, builder.CreateGlobalStringPtr(": loop_" + name_suffix));
+  }
   for (const auto *sub : ast->block) {
     on_ast(sub);
   }
+  pointerUpdated();
   builder.CreateBr(guard);
   builder.SetInsertPoint(after);
-  builder.CreateCall(dmsg, builder.CreateGlobalStringPtr(": afterloop_" + name_suffix));
+  if (debugMode) {
+    builder.CreateCall(dmsg, builder.CreateGlobalStringPtr(": afterloop_" + name_suffix));
+  }
   return nullptr;
 }
 
@@ -194,6 +217,37 @@ Module *LLVMGenerator::codegen(const std::vector<AST *> &asts) {
 }
 
 void LLVMGenerator::collect(value_t value) { }
+
+void LLVMGenerator::unitUpdated() {
+  uval_cache = nullptr;
+}
+
+void LLVMGenerator::pointerUpdated() {
+  pval_cache = nullptr;
+  uval_cache = nullptr;
+  unit_cache = nullptr;
+}
+
+llvm::Value *LLVMGenerator::getPtr() {
+  if (pval_cache == nullptr) {
+    pval_cache = builder.CreateLoad(i32Ty, ptr);
+  }
+  return pval_cache;
+}
+
+llvm::Value *LLVMGenerator::getCurrentUnit() {
+  if (uval_cache == nullptr) {
+    uval_cache = builder.CreateLoad(i8Ty, getCurrentUnitPtr());
+  }
+  return uval_cache;
+}
+
+llvm::Value *LLVMGenerator::getCurrentUnitPtr() {
+  if (unit_cache == nullptr) {
+    unit_cache = builder.CreateInBoundsGEP(units, getPtr());
+  }
+  return unit_cache;
+}
 
 }
 }
